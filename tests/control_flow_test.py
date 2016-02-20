00aeb8011d582ed94c9fa338999986bfdbd05280
AV <alphavibe@gmail.com>
2016-02-20 18:14:04 -0500

    Add test for stop on error

--- tests/control_flow_test.py
+++ tests/control_flow_test.py
@@ -8,7 +8,7 @@ from py_validator import *
 
 class ControlFlowTest(unittest.TestCase):
 
-    def test_global_stop_works(self):
+    def test_global_stop_on_first_error_works(self):
         response = PyValidator()\
             .stop_on_first_error()\
             .rules_for('foo1', lambda o: o['foo1'])\
@@ -22,7 +22,7 @@ class ControlFlowTest(unittest.TestCase):
         self.assertTrue(not response.is_valid)
         self.assertEqual(len(response.errors), 2)
 
-    def test_conditional_works(self):
+    def test_stop_on_first_error__works(self):
         response = PyValidator()\
             .rules_for('foo1', lambda o: o['foo1'])\
                 .stop_on_first_error()\
@@ -35,6 +35,22 @@ class ControlFlowTest(unittest.TestCase):
 
         self.assertTrue(not response.is_valid)
         self.assertEqual(len(response.errors), 3)
+
+    def test_stop_on_error__works(self):
+        response = PyValidator()\
+            .rules_for('foo1', lambda o: o['foo1'])\
+                .not_none()\
+                .is_int()\
+            .rules_for('foo2', lambda o: o['foo2'])\
+                .not_none()\
+                .is_string()\
+                .is_between(5, 10)\
+                    .stop_on_error()\
+                .is_equals(16)\
+            .validate({'foo1': 15, 'foo2': 20})
+
+        self.assertTrue(not response.is_valid)
+        self.assertEqual(len(response.errors), 2)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
